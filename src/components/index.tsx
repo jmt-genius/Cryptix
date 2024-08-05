1512f8370afe79a3ea5b7a28cb38c7c40304efdf
Akkilesh-A <akkilalagar05@gmail.com>
2024-08-05 22:26:38 +0530

    resolve merge conflict

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,6 @@
 import Heading from "./Heading";
 import Text from "./Text";
 import SubHeading from "./SubHeading"
-
 import { ThemeProvider } from "./ThemeProvider";
 
 export {Heading, Text,SubHeading, ThemeProvider }
